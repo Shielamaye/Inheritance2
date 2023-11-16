d698c08b4ddfc6de53cfa4493a2fd2edd8188f05
Shielamaye <143503450+Shielamaye@users.noreply.github.com>
2023-11-16 18:47:41 +0800

    Add files via upload

---
+++ Inheritance/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication5 {
+   
+
+    
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        
+        vehicle.brand ="Honda";
+        vehicle.modelName = "Camaro";
+        System.out.println("Brand:" + vehicle.brand);
+        System.out.println("Model:" + vehicle.modelName);
+        
+    }
+    
+}
---
+++ Inheritance/src/javaapplication5/Vehicle.java
@@ -0,0 +1,14 @@
+
+package javaapplication5;
+
+
+public class Vehicle {
+    String brand;
+    String modelName;
+    
+    void honk(){
+    System.out.println("Lol Lol");
+    
+    }
+    
+}
